455a37cb39b674cecb3f8f51016988a3e286621b
Dang Chau Bao Van <44543505+DangChauBaoVan@users.noreply.github.com>
2024-07-08 15:56:21 +0700

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import  {BsGithub} from "react-icons/bs"
 const HeaderSocials = () => {
   return (
     <div className="headerSocials">
-      <a href="https://www.linkedin.com/in/%C4%91%E1%BA%B7ng-ch%C3%A2u-b%E1%BA%A3o-v%C4%83n-67118b1ab" target="_blank"><BsLinkedin/></a>
+      <a href="www.linkedin.com/in/dangchaubaovan" target="_blank"><BsLinkedin/></a>
       <a href="https://github.com/DangChauBaoVan" target="_blank"><BsGithub/></a>
     </div>
   )
